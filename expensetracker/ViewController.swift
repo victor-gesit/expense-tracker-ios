fddf96ed2c164155bc60755691a8355784fd0d40
Victor Idongesit <victoridongesit@gmail.com>
2020-08-20 20:17:27 +0100

    setup authentication pages in storyboard

---
+++ expensetracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  expensetracker
+//
+//  Created by Victor Idongesit on 20/08/2020.
+//  Copyright © 2020 Victor Idongesit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
